402eeb77163b9399455989ffe02e7002bff0317f
aronunlabeled <xaron97x@hotmail.com>
2021-10-08 18:03:49 -0400

    first commit

---
+++ AronGibsonLab10Circle/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AronGibsonLab10Circle
+{
+
+    class Circle
+    {
+        private double radius { get; set; }
+
+        public Circle(double radius) {
+            this.radius = radius;
+        }
+        public double CalculateCircumference() {
+            return 2 * Math.PI * radius;
+        }
+        public string CalculateFormattedCircumference() {
+
+            return FormatNumber(CalculateCircumference());
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+        private string FormatNumber(double x) {
+            return string.Format("{0:N2}",x);
+        }
+    }
+}
---
+++ AronGibsonLab10Circle/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace AronGibsonLab10Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int circlesCalculated=0; 
+            int num=0;
+            string again="y";
+            do
+            {
+                circlesCalculated += 1;
+                Circle c = new Circle(IntValidator());    
+                Console.WriteLine($"Circumference {c.CalculateFormattedCircumference()}");
+                Console.WriteLine($"Area: {c.CalculateFormattedArea()}");
+                again = AgainValidator();
+            } while (again=="y");
+
+            Console.WriteLine($"goodbye, You created {circlesCalculated} circle objects");
+
+        }
+        public static int IntValidator()
+        {
+            do
+            {
+                Console.WriteLine("enter radius ");
+                try
+                {
+                    int num = int.Parse(Console.ReadLine());
+                    return num;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Must enter a number.");              
+                }
+            } while (true);     
+        }
+
+        public static string AgainValidator()
+        {
+            do
+            {
+                Console.WriteLine("Enter radius for another circle? y or n.");
+                string again = Console.ReadLine();
+                if (again == "y")
+                    return again;
+                else if (again == "n")
+                    return again;
+                Console.WriteLine("Must enter y or n");
+            } while (true);
+        }
+
+
+    }
+}
